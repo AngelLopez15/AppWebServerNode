e196bd4e0b70b671d68acb37cc879bffa07be921
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-17 01:32:46 -0500

    Parti los HTML en multiples archivos hbs

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require('express')
 // declaramos una constante app que es producto de la funcion express
 const app = express()
 
+// importando  el hbs
+const hbs = require('hbs')
+
+
 // creando un midleware para servir paginas estaticas
 // debemos comprender que todo lo que esta en la carpeta public va a ser de dominio publico
+// con __dirname estamos diciendo que toda la ruta anterior le concatene el /public
 app.use(express.static(__dirname + '/public'))
 
 // declarando el Express HBS engine
+hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
 
 //renderizando la pagina desde el midle y el archivo hbs 
@@ -17,7 +23,12 @@ app.get('/', (req, res) =>{
         anio: new Date().getFullYear()
     })
 })
-
+app.get('/features', (req, res) => {
+    res.render('features',{
+        name:'Angel',
+        anio: new Date().getFullYear()
+    })
+} )
 // // configuramos una solicitud GET cuando el path sea un /
 // app.get('/', (req, res) => {
 //     const salida = {
